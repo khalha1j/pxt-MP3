92816a40d2711a46005ed1c89c9d4a31b6b3d499
Husain Ali <khalha1j@gmail.com>
2019-02-01 02:08:25 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 */
 
 
-//% weight=0 color=#b9a0e6 icon="\uf001" block="MP3 Player-H"
+//% weight=0 color=#b9a0e6 icon="\uf001" block="MP3 Player-HA"
 namespace dfplayer {
     serial.onDataReceived("E", () => {
     })
